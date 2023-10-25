2d3b17bcf2f03a0affa784c4a5e25923e839f3b2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 13:51:02 +0100

    chore: add file to import data to db

---
+++ data/import-data.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Movie = require("../models/MovieModel");
+// const User = require('../models/userModel');
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log("DB connection successful"));
+
+const movies = JSON.parse(fs.readFileSync(`${__dirname}/movies.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/user.json`, "utf-8"));
+
+const importData = async () => {
+  try {
+    await Movie.create(movies);
+    // await User.create(users, { validateBeforeSave: false });
+
+    console.log("Data successfully loaded");
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Movie.deleteMany();
+
+    // await User.deleteMany();
+    console.log("Data successfully deleted");
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+if (process.argv.at(2) === "--import") {
+  importData();
+} else if (process.argv.at(2) === "--delete") {
+  deleteData();
+}
