26cf3eb40ec7196af95ad39c3e82492e0b16655e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 07:42:29 +0100

    feat: add factory function to get one

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,12 +1,27 @@
 const catchAsync = require("../utils/catchAsync");
 
-exports.getAll = async (Modal) => {
+exports.getAll = (Model) => {
   catchAsync(async (req, res) => {
-    const doc = await Modal.find();
+    const doc = await Model.find();
 
     res.status(200).json({
       status: "success",
       result: doc.length,
+      data: {
+        document: doc,
+      },
+    });
+  });
+};
+
+exports.getOne = (Model) => {
+  catchAsync(async (req, res, next) => {
+    const doc = Model.findById(req.params.id);
+
+    if (!doc) return next();
+
+    res.status(200).json({
+      status: "success",
       data: {
         document: doc,
       },
