71c11d1c9091fe19df6d6c531425abdeb49f2269
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 08:52:32 +0100

    feat: add filter feature to the api

---
+++ utils/ApiFeatues.js
@@ -0,0 +1,26 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    (this.query = query), (this.queryString = queryString);
+  }
+
+  filter() {
+    const queryObject = { ...this.queryString };
+
+    const excludedQuery = ["sort", "limit", "page", "field"];
+
+    excludedQuery.forEach((el) => delete queryObject[el]);
+
+    // Advanced filtering
+
+    let queryStr = JSON.stringify(queryObject).replace(
+      /\b(lt|lte|gt|gte)\b/g,
+      (match) => `$${match}`
+    );
+
+    queryStr = JSON.parse(queryStr);
+
+    this.query = this.query.find(queryStr);
+
+    return this;
+  }
+}
