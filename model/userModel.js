a2895b2d05f73f91e6e0a77db940df4ca0920adc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 15:29:40 +0100

    feat: add userscema

---
+++ model/userModel.js
@@ -0,0 +1,39 @@
+const validator = require("validator");
+
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minlength: [2, "Name should consist at least 2 characters"],
+    maxlength: [40, "Name should at most consist 40 characters"],
+    required: [true, "Please tell us yout name"],
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "This email already exists, please try another email"],
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+
+  password: {
+    type: String,
+    minLength: [8, "password should be at least 8 charaters long"],
+    required: [true, "Please provide a password"],
+  },
+
+  passwordConfirm: {
+    type: String,
+
+    required: ["please confirm your password"],
+
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+
+      message: "Passwords are not the same",
+    },
+  },
+});
