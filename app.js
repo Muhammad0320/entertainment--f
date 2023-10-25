66fe96c10dec519c94422759382e5b9012447982
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 05:04:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+const express = require('express')
+
+const morgan = require('morgan')
+
+const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+
+
+    morgan('dev') 
+
+
+}
+
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,37 @@
+const dotenv = require("dotenv");
+
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+  console.log("ERROR 🔥🔥💩, UNCAUGHT EXCEPTION");
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log("DB connection succesful"));
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port} `);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("ERROR 🔥🔥💩", "UNHANDLED REJECTION");
+
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
