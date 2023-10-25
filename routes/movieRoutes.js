7c659a3b52b6f4a49f8a9001db18528b88cf9bcd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 14:22:20 +0100

    feat: add route to get all ans create new movie

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getMovies, createMovie } = require("../controllers/movieController");
 
 const router = express.Router();
+
+router.route("/").get(getMovies).post(createMovie);
 
 module.exports = router;
