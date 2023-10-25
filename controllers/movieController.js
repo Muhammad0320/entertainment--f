4ecd9a72c6f88a1be7850599461864932c25991b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 14:19:32 +0100

    feat: add controller function to update and delete movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,9 +1,19 @@
 const Movie = require("../model/movieModel");
 
-const { createOne, getAll, getOne } = require("./handlerFactory");
+const {
+  createOne,
+  getAll,
+  getOne,
+  updateOne,
+  deleteOne,
+} = require("./handlerFactory");
 
 exports.createMovie = createOne(Movie);
 
 exports.getMovies = getAll(Movie);
 
-exports.getOne = getOne(Movie);
+exports.getMovie = getOne(Movie);
+
+exports.updateMovie = updateOne(Movie);
+
+exports.deleteMovie = deleteOne(Movie);
