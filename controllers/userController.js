10ed09232bdbac65434afeebb44e2ca2ae1d714c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 15:37:38 +0100

    feat: add the CRUD operation to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const User = require("../model/userModel");
-const { createOne, getAll, getOne } = require("./handlerFactory");
+const {
+  createOne,
+  getAll,
+  getOne,
+  updateOne,
+  deleteOne,
+} = require("./handlerFactory");
 
 exports.createUser = createOne(User);
 
 exports.getUsers = getAll(User);
 
 exports.getUSer = getOne(User);
+
+exports.updateUser = updateOne(User);
+
+exports.deleteUser = deleteOne(User);
