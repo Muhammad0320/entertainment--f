6d0ae48a5019626a1bce028f0d3489b898e3a85b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-25 09:05:54 +0100

    feat: add api feature to paginate output

--- utils/ApiFeatues.js
+++ utils/ApiFeatues.js
@@ -47,6 +47,18 @@ class ApiFeatures {
 
     return this;
   }
+
+  paginate() {
+    const limit = this.queryString.limit * 1 || 100;
+
+    const page = this.queryString.page * 1 || 1;
+
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
 }
 
 module.exports = ApiFeatures;
