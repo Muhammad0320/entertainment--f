eba4921141ea124c793d48895c3de8b2feca65f8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 10:43:17 +0100

    chore: add middleware to create bookmark logically

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,1 +1,7 @@
-const addUserMovieId = (req, res, next) => {};
+exports.addUserMovieId = (req, res, next) => {
+  if (!req.body.user) req.body.user = req.user._id;
+
+  if (!req.body.movie) req.body.movie = req.params.movieId;
+
+  next();
+};
