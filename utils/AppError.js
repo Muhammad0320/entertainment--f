72a47145bd467acdc67250e6fcc51de1871d412a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 06:19:03 +0100

    feat add the appError class

--- utils/AppError.js
+++ utils/AppError.js
@@ -1,3 +1,13 @@
-class AppError {}
+class AppError {
+  constructor(message, statusCode) {
+    this.message = message;
+
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
+    this.isOperational = true;
+
+    this.statusCode = statusCode;
+  }
+}
 
 module.exports = AppError;
