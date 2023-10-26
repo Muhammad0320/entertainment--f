9be86ecc81b58aa4c282750ab457f195b5e48094
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 10:55:37 +0100

    chore: add a route to navigate to the bookmark router on a specific endpoint

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -12,12 +12,15 @@ const router = express.Router();
 
 router.route("/:id").get(getMovie);
 
-router
-  .route("/")
-  .post(protect, restrictTo("admin"), createMovie)
-  .get(getMovies);
+router.route("/id").get(getMovies);
 
-router.use(protect, restrictTo("admin"));
+router.use(protect);
+
+router.use("/bookmark/:movieId", "");
+
+router.route("/").post(restrictTo("admin"), createMovie);
+
+router.use(restrictTo("admin"));
 
 router
   .route("/:id")
