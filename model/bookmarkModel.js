7d632ed81cd3683acfc9bf5efd5ecea4a5624c22
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 10:35:18 +0100

    feat: add bookmark scema type definition

--- model/bookmarkModel.js
+++ model/bookmarkModel.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose");
 
-const bookmarkSchema = new mongoose.Schema({});
+const bookmarkSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User"
+  },
+
+  movie: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Movie"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
