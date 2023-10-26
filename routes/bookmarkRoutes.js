f3109955ee00519d9b32afc1f6f888d3ba80c3e5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 11:06:38 +0100

    chore: add crud operation routes to bookmark

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const {
   getBookmarks,
-  getBookmark
-} = require("../controllers/bookmarkController");
+  getBookmark,
+  createBookmark,
 
-const router = express.Router();
+  deleteBookmark,
+  addUserMovieId
+} = require("../controllers/bookmarkController");
+const { protect } = require("../controllers/authController");
+
+const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getBookmarks);
 
 router.route("/:id").get(getBookmark);
+
+router.use(protect);
+
+router.route("/").post(addUserMovieId, createBookmark);
+
+router.route("/:id").delete(deleteBookmark);
 
 module.exports = router;
