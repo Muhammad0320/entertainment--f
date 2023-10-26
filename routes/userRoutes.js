04b6f2865d03e772c05c8b9cd9089d5e60acc0cb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 04:48:17 +0100

    chore: add routes to get and create user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const { getUsers, createUser } = require("../controllers/userController");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(getUsers)
+  .post(createUser);
 
 module.exports = router;
