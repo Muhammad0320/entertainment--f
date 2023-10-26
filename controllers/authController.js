c2a06c090e30a429bda97db2379ceea5c3cb0b53
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 04:10:27 +0100

    chore: add a function to send jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,15 @@
 const jwt = require("jsonwebtoken");
+
+const sendJwt = (res, user, status) => {
+  const token = jwt.sign(user._id, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
+  res.status(status).json({
+    status: "success",
+
+    token,
+
+    user
+  });
+};
