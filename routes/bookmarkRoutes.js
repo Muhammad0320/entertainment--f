79d9ba5088e73f19fd3f8a245971fa791fc8737b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 11:02:12 +0100

    chore: add the simple routes for bookmarks

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const {
+  getBookmarks,
+  getBookmark
+} = require("../controllers/bookmarkController");
 
 const router = express.Router();
+
+router.route("/").get(getBookmarks);
+
+router.route("/:id").get(getBookmark);
 
 module.exports = router;
