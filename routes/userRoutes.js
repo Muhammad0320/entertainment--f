b4c195084251fc2cf1c3447a5cb8789096f9cce6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 04:49:42 +0100

    chore: add other routed to complete crud operation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { getUsers, createUser } = require("../controllers/userController");
+const {
+  getUsers,
+  createUser,
+  getUSer,
+  updateUser,
+  deleteUser
+} = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -7,5 +13,11 @@ router
   .route("/")
   .get(getUsers)
   .post(createUser);
+
+router
+  .route("/:id")
+  .get(getUSer)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
