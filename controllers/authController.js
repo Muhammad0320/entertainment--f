b74da83f2083b21764005f5f649b61dfdf54ddca
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-26 08:31:29 +0100

    feat: add cookie sending implementation

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,14 @@ const sendJwt = (res, user, status) => {
     expiresIn: process.env.JWT_EXPIRES_IN
   });
 
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true
+  });
+
   res.status(status).json({
     status: "success",
 
